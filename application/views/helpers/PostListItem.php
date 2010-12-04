72a371eb7cff5e63af364518416ed8f5fd35d63c
Shahar Evron <shahar.evron@gmail.com>
2010-12-04 12:44:15 +0200

    Making 'read more' a link to the post

--- application/views/helpers/PostListItem.php
+++ application/views/helpers/PostListItem.php
@@ -10,15 +10,16 @@ class Stoa_View_Helper_PostListItem extends Zend_View_Helper_Abstract
      */
     public function postListItem(Stoa_Model_Post $post)
     {
+        $postUrl = $this->view->baseUrl . '/post/' . $post->getId();
         $html = '<div class="post">' .
-                    '<h3><a href="' . $this->view->baseUrl . '/post/' . $post->getId() . '">' . 
+                    '<h3><a href="' . $postUrl . '">' . 
                         htmlspecialchars($post->title) . '</a></h3>' .
                     '<div class="post-info">' . 
                         '<span>' . $this->view->timeFormat($post->created_at) . '</span>' .
                         '<span>tags: ' . $this->view->tagsFormat($post->tags) . '</span>' . 
                     '</div>' .
                     '<div class="content">' . 
-                        $this->_previewSubString($post->content) .
+                        $this->_previewSubString($post->content, $postUrl) .
                     '</div>' . 
                 '</div>';
         
@@ -29,14 +30,15 @@ class Stoa_View_Helper_PostListItem extends Zend_View_Helper_Abstract
      * Get a preview of the post's content
      * 
      * @param  string  $text
+     * @param  string  $url
      * @param  integer $limit
      * @return string
      */
-    protected function _previewSubString($text, $limit = 1000)
+    protected function _previewSubString($text, $url, $limit = 1000)
     {
         if (strlen($text) > $limit) {
             $text = $this->view->contentFormat(substr($text, 0, strpos($text, "\n", $limit))) . "\n" . 
-                '<span class="more">[read more...]</a>'; 
+                '<a href="' . $url . '" class="read-more">[read more...]</a>'; 
         } else {
             $text = $this->view->contentFormat($text);
         } 
